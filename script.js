0d61690518090e1e607aa5de4aee2f015e28ee94
Egor <vaspovas@gmail.com>
2022-05-31 13:41:25 +0300

    сделал рефакторинг

--- script.js
+++ script.js
@@ -1,37 +1,57 @@
-const counter = document.querySelector('.counter')
-let hod = 0
+const fields = document.querySelector('.fields')
+const crossHTML =  '<div class="x"></div>'
+const zeroHTML = '<div class="circle"></div>'
+let step = 0
+
 function ticTacToe(event) {
     if(event.target.classList.contains('field')){
-        if(hod % 2 === 0){
-            event.target.innerHTML = '<div class="x"></div>'
+        if(step % 2 === 0){
+            event.target.innerHTML = crossHTML
         }else {
-            event.target.innerHTML = '<div class="circle"></div>'
+            event.target.innerHTML = zeroHTML
         }
-        hod++
+        step++
     }
     winnerCheck()
 }
-counter.onclick = ticTacToe
+fields.onclick = ticTacToe
+
+const winCombinations = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,4,8],
+    [2,4,6],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8]
+]
 
 const allField = document.getElementsByClassName('field')
-function winnerCheck() {
-    if (allField[0].innerHTML === '<div class="x"></div>' && allField[1].innerHTML === '<div class="x"></div>' && allField[2].innerHTML === '<div class="x"></div>') alert('Победили крестики')
-    if (allField[3].innerHTML === '<div class="x"></div>' && allField[4].innerHTML === '<div class="x"></div>' && allField[5].innerHTML === '<div class="x"></div>') alert('Победили крестики')
-    if (allField[0].innerHTML === '<div class="x"></div>' && allField[4].innerHTML === '<div class="x"></div>' && allField[8].innerHTML === '<div class="x"></div>') alert('Победили крестики')
-    if (allField[6].innerHTML === '<div class="x"></div>' && allField[7].innerHTML === '<div class="x"></div>' && allField[8].innerHTML === '<div class="x"></div>') alert('Победили крестики')
-    if (allField[2].innerHTML === '<div class="x"></div>' && allField[4].innerHTML === '<div class="x"></div>' && allField[6].innerHTML === '<div class="x"></div>') alert('Победили крестики')
-    if (allField[0].innerHTML === '<div class="x"></div>' && allField[3].innerHTML === '<div class="x"></div>' && allField[6].innerHTML === '<div class="x"></div>') alert('Победили крестики')
-    if (allField[1].innerHTML === '<div class="x"></div>' && allField[4].innerHTML === '<div class="x"></div>' && allField[7].innerHTML === '<div class="x"></div>') alert('Победили крестики')
-    if (allField[2].innerHTML === '<div class="x"></div>' && allField[5].innerHTML === '<div class="x"></div>' && allField[8].innerHTML === '<div class="x"></div>') alert('Победили крестики')
-    if (allField[0].innerHTML === '<div class="circle"></div>' && allField[1].innerHTML === '<div class="circle"></div>' && allField[2].innerHTML === '<div class="circle"></div>') alert('Победили нолики')
-    if (allField[3].innerHTML === '<div class="circle"></div>' && allField[4].innerHTML === '<div class="circle"></div>' && allField[5].innerHTML === '<div class="circle"></div>') alert('Победили нолики')
-    if (allField[0].innerHTML === '<div class="circle"></div>' && allField[4].innerHTML === '<div class="circle"></div>' && allField[8].innerHTML === '<div class="circle"></div>') alert('Победили нолики')
-    if (allField[6].innerHTML === '<div class="circle"></div>' && allField[7].innerHTML === '<div class="circle"></div>' && allField[8].innerHTML === '<div class="circle"></div>') alert('Победили нолики')
-    if (allField[2].innerHTML === '<div class="circle"></div>' && allField[4].innerHTML === '<div class="circle"></div>' && allField[6].innerHTML === '<div class="circle"></div>') alert('Победили нолики')
-    if (allField[0].innerHTML === '<div class="circle"></div>' && allField[3].innerHTML === '<div class="circle"></div>' && allField[6].innerHTML === '<div class="circle"></div>') alert('Победили нолики')
-    if (allField[1].innerHTML === '<div class="circle"></div>' && allField[4].innerHTML === '<div class="circle"></div>' && allField[7].innerHTML === '<div class="circle"></div>') alert('Победили нолики')
-    if (allField[2].innerHTML === '<div class="circle"></div>' && allField[5].innerHTML === '<div class="circle"></div>' && allField[8].innerHTML === '<div class="circle"></div>') alert('Победили нолики')
+
+function getIsCrossesWin() {
+    return winCombinations.some((winCombination)=> {
+        return  allField[winCombination[0]].innerHTML === crossHTML &&
+                allField[winCombination[1]].innerHTML === crossHTML &&
+                allField[winCombination[2]].innerHTML === crossHTML
+    })
 }
+
+function getIsZeroesWin() {
+    return winCombinations.some((winCombination)=> {
+        return  allField[winCombination[0]].innerHTML === zeroHTML &&
+            allField[winCombination[1]].innerHTML === zeroHTML &&
+            allField[winCombination[2]].innerHTML === zeroHTML
+    })
+}
+
+function winnerCheck() {
+    const textCrosses = 'Победили крестики'
+    const textZeroes = 'Победили нолики'
+    if(getIsCrossesWin()) alert(textCrosses)
+    else if(getIsZeroesWin()) alert(textZeroes)
+}
+
 let btm = document.querySelector('.go-game')
 function clear() {
     for (let i = 0; i < allField; i++) {
