15206f4f139110bfaa81244e0ec93f2e8c0e1d68
Egor <vaspovas@gmail.com>
2022-06-04 15:18:09 +0300

    доделал крестики-нолики

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
 const fields = document.querySelector('.fields')
 const crossHTML =  '<div class="x"></div>'
+const crossRedHTML = '<div class="x2"></div>'
 const zeroHTML = '<div class="circle"></div>'
 let step = 0
 let crossesWin = 0
 let zeroesWin = 0
 let drawWin = 0
 let hod = 0
+let isGameOver = false
 const hodXOrO = document.querySelector('.hodXOrO')
 function ticTacToe(event) {
-    if(event.target.classList.contains('field')){
-        if(step % 2 === 0){
-            event.target.innerHTML = crossHTML
-            hodXOrO.innerHTML = 'O'
-        }else {
-            event.target.innerHTML = zeroHTML
-            hodXOrO.innerHTML = 'X'
+    if(!isGameOver){
+        if(event.target.classList.contains('field')){
+            if(step % 2 === 0){
+                event.target.innerHTML = crossHTML
+                hodXOrO.innerHTML = 'O'
+            }else {
+                event.target.innerHTML = zeroHTML
+                hodXOrO.innerHTML = 'X'
+            }
+            step++
         }
-        step++
     }
     winnerCheck()
-    draw.innerHTML = drawWin
-    crosses.innerHTML = crossesWin
-    zeroes.innerHTML = zeroesWin
 }
+
+
 fields.onclick = ticTacToe
 
 const winCombinations = [
@@ -38,31 +41,40 @@ const winCombinations = [
 
 const allField = document.getElementsByClassName('field')
 
-function getIsCrossesWin() {
-    return winCombinations.some((winCombination)=> {
-        if ( allField[winCombination[0]].innerHTML === crossHTML &&
+function getBacklightCrosses() {
+    return winCombinations.some((winCombination) => {
+        if (allField[winCombination[0]].innerHTML === crossHTML &&
             allField[winCombination[1]].innerHTML === crossHTML &&
             allField[winCombination[2]].innerHTML === crossHTML) {
-            allField[winCombination[0]].style.backgroundColor = 'red'
-            allField[winCombination[1]].style.backgroundColor = 'red'
-            allField[winCombination[2]].style.backgroundColor = 'red'
+            allField[winCombination[0]].innerHTML = crossRedHTML
+            allField[winCombination[1]].innerHTML = crossRedHTML
+            allField[winCombination[2]].innerHTML = crossRedHTML
         }
-        return allField[winCombination[0]].innerHTML === crossHTML &&
-               allField[winCombination[1]].innerHTML === crossHTML &&
-               allField[winCombination[2]].innerHTML === crossHTML
+    })
+}
+
+function getBacklightZeroes() {
+    return winCombinations.some((winCombination) => {
+        if (allField[winCombination[0]].innerHTML === zeroHTML &&
+            allField[winCombination[1]].innerHTML === zeroHTML &&
+            allField[winCombination[2]].innerHTML === zeroHTML) {
+            allField[winCombination[0]].firstChild.style.backgroundColor = 'red'
+            allField[winCombination[1]].firstChild.style.backgroundColor = 'red'
+            allField[winCombination[2]].firstChild.style.backgroundColor = 'red'
+        }
+    })
+}
+function getIsCrossesWin() {
+    return winCombinations.some((winCombination) => {
+         return allField[winCombination[0]].innerHTML === crossHTML &&
+                allField[winCombination[1]].innerHTML === crossHTML &&
+                allField[winCombination[2]].innerHTML === crossHTML
     })
 
 }
 
 function getIsZeroesWin() {
-    return winCombinations.some((winCombination)=> {
-        if( allField[winCombination[0]].innerHTML === zeroHTML &&
-            allField[winCombination[1]].innerHTML === zeroHTML &&
-            allField[winCombination[2]].innerHTML === zeroHTML){
-            allField[winCombination[0]].style.backgroundColor = 'red'
-            allField[winCombination[1]].style.backgroundColor = 'red'
-            allField[winCombination[2]].style.backgroundColor = 'red'
-        }
+    return winCombinations.some((winCombination) => {
         return allField[winCombination[0]].innerHTML === zeroHTML &&
                allField[winCombination[1]].innerHTML === zeroHTML &&
                allField[winCombination[2]].innerHTML === zeroHTML
@@ -76,25 +88,30 @@ function winnerCheck() {
     const textDraw = 'Ничья'
 
     if(getIsCrossesWin()){
+        getBacklightCrosses()
         alert(textCrosses)
         crossesWin++
         strWin.innerHTML = 'Crosses win'
         strWin.style.visibility =  'visible'
+        isGameOver = true
     }
     else if (getIsZeroesWin()){
+        getBacklightZeroes()
         alert(textZeroes)
         zeroesWin++
         strWin.innerHTML = 'Zeroes win'
         strWin.style.visibility =  'visible'
+        isGameOver = true
     }
     else if(!getIsZeroesWin() && !getIsZeroesWin()){
         hod++
     }
-    if(hod === 9){
+    if(hod === 9 && !isGameOver){
         alert(textDraw)
         drawWin++
         strWin.innerHTML = 'Draw'
         strWin.style.visibility =  'visible'
+        isGameOver = true
     }
 }
 
